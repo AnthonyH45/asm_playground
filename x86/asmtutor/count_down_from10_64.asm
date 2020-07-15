7fdd79f6c8130ff1d1b7a72a5cedfd242a6cbe7b
Anthony Hallak <ahall012@ucr.edu>
2020-07-14 22:47:47 -0700

    seg faulting lol

--- x86/asmtutor/count_down_from10_64.asm
+++ x86/asmtutor/count_down_from10_64.asm
@@ -7,12 +7,38 @@ _start:
     call sprint                             ; print msg
     mov rax, newline
     call sprint
-    ;call count_down
+    call count_down
     jmp end
+
+count_down:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+
+    xor rax, rax
+    xor rbx, rbx
+    xor rcx, rcx
+    xor rdx, rdx
+
+    add rbx, 0x09
+    print_loop:
+        add rax, rbx
+        add rax, 0x30
+        call sprint
+        dec rbx
+        cmp rbx, 0x0
+        jne print_loop
+    
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    ret
 
 end:
     call quit
 
     section .data
-msg1    db 'Counting down from 10',0x0
+msg1    db 'Counting down from 9',0x0
 newline db '',0xa,0x0

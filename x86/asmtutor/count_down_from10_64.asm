052f6facd8dbab1555c11964cb804e352ceb297b
Anthony Hallak <ahall012@ucr.edu>
2020-07-15 18:24:37 -0700

    prints nothing, but doesnt seg faul!

--- x86/asmtutor/count_down_from10_64.asm
+++ x86/asmtutor/count_down_from10_64.asm
@@ -1,42 +1,65 @@
-    %include 'functions64.asm'
     section .text
     global _start
 
 _start:
-    mov rax, msg1                           ; rax <- "Counting down from 10"
-    call sprint                             ; print msg
-    mov rax, newline
-    call sprint
+    mov rdx, len
+    mov rcx, msg1
+    mov rbx, 1
+    mov rax, 4
+    int 0x80
+
+    mov rdx, 1
+    mov rcx, newline
+    mov rbx, 1
+    mov rax, 4
+    int 0x80
+    
     call count_down
-    jmp end
+    jmp quit
+
 
 count_down:
     push rax
+    push rbx
     push rcx
+    push rdx
 
     xor rax, rax
+    xor rbx, rbx
     xor rcx, rcx
+    xor rdx, rdx
 
     add rcx, 0x09
     print_loop:
-        mov rax, rcx
-        add rax, 0x30
-        push rax
-        mov rax, rsp
-        call sprintLF
-        pop rax
+        push rcx
+        add rcx, 0x30
+        mov rdx, 1
+        mov rbx, 1
+        mov rax, 4
+        int 0x80
 
+        mov rdx, 1
+        mov rcx, newline
+        mov rbx, 1
+        mov rax, 4
+        int 0x80
+
+        pop rcx
         dec rcx
         cmp rcx, 0x0
-        jne print_loop
+        jge print_loop
 
+    pop rdx
     pop rcx
+    pop rbx
     pop rax
     ret
 
-end:
-    call quit
+quit:
+    mov rax, 1
+    int 0x80
 
     section .data
 msg1    db 'Counting down from 9',0x0
+len     equ $ - msg1
 newline db '',0xa,0x0
